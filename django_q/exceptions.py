e5077cbdf58a6704efec4172219499d6ae2c1c49
GDay <1939656+GDay@users.noreply.github.com>
2024-06-23 22:12:44 +0200

    add docs

--- django_q/exceptions.py
+++ django_q/exceptions.py
@@ -1,5 +1,3 @@
-import signal
-
 class TimeoutException(SystemExit):
     """
     Exception for when a worker takes too long to complete a task
@@ -8,22 +6,3 @@ class TimeoutException(SystemExit):
     pass
 
 
-class TimeoutHandler:
-    def __init__(self, timeout: int):
-        self._timeout = timeout
-
-    def raise_timeout_exception(self, signum, frame):
-        raise TimeoutException(f"Task exceeded maximum timeout value ({self._timeout} seconds)")
-
-    def __enter__(self):
-        if self._timeout == -1:
-            return
-        signal.signal(signal.SIGALRM, self.raise_timeout_exception)
-        signal.alarm(self._timeout)
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        if self._timeout == -1:
-            return
-        """When getting out of the timeout, reset the alarm, so it won't trigger"""
-        signal.alarm(0)
-        signal.signal(signal.SIGALRM, signal.SIG_DFL)
